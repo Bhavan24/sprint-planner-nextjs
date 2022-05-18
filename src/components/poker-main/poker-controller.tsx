6cb648fd0a52631d4d0005b03c1f9efdeb5cd41e
bhavan <bhavanl@techprovint.com>
2022-05-18 15:52:58 +0530

    implement poker controller

--- src/components/poker-main/poker-controller.tsx
+++ src/components/poker-main/poker-controller.tsx
@@ -1,10 +1,17 @@
-import { Stack } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Box, Text, IconButton, Stack, Tooltip, useToast } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { FaLink, FaRegEye } from 'react-icons/fa';
+import { VscDebugRestart } from 'react-icons/vsc';
+import { MdExitToApp } from 'react-icons/md';
 
 const PokerController = () => {
     const router = useRouter();
     const [gameId, setGameId] = useState('');
+
+    // toast
+    const toast = useToast();
 
     useEffect(() => {
         if (!router.isReady) return;
@@ -15,12 +22,91 @@ const PokerController = () => {
     const copyInviteLink = () => {
         const url = `${window.location.origin}/sprint-poker?join=${gameId}`;
         navigator.clipboard.writeText(url);
+        toast({
+            position: 'top-right',
+            title: 'Invite Link copied to clipboard!',
+            status: 'success',
+            isClosable: true,
+        });
     };
 
     return (
         <>
-            <Stack spacing="6">
-                <p>PokerController</p>
+            <Stack spacing="6" alignItems="center">
+                <Box
+                    maxW="max-content"
+                    maxH="lg"
+                    borderWidth="2px"
+                    borderRadius="lg"
+                    m="1"
+                >
+                    <Box p="6">
+                        <Box
+                            color="gray.500"
+                            fontWeight="semibold"
+                            letterSpacing="wide"
+                            fontSize="xs"
+                            textTransform="uppercase"
+                            alignContent="center"
+                            gap="1"
+                        >
+                            <Text>Session Name</Text>
+                            <Text>Session Status</Text>
+                        </Box>
+                        <Box
+                            mt="1"
+                            fontWeight="semibold"
+                            as="h4"
+                            lineHeight="tight"
+                            noOfLines={1}
+                        >
+                            Average : 2.5
+                        </Box>
+                        <Box gap="2" p="2">
+                            <Tooltip label="Reveal Cards">
+                                <IconButton
+                                    colorScheme="blue"
+                                    aria-label="Reveal"
+                                    icon={<FaRegEye />}
+                                    isRound
+                                    size="lg"
+                                    m={2}
+                                />
+                            </Tooltip>
+                            <Tooltip label="Restart Session">
+                                <IconButton
+                                    colorScheme="blue"
+                                    aria-label="Restart"
+                                    icon={<VscDebugRestart />}
+                                    isRound
+                                    size="lg"
+                                    m={2}
+                                />
+                            </Tooltip>
+                            <Tooltip label="Exit Session">
+                                <IconButton
+                                    colorScheme="blue"
+                                    aria-label="Exit"
+                                    icon={<MdExitToApp />}
+                                    isRound
+                                    size="lg"
+                                    m={2}
+                                />
+                            </Tooltip>
+                            <Tooltip label="Copy Invitation Link">
+                                <IconButton
+                                    colorScheme="blue"
+                                    aria-label="Invite"
+                                    icon={<FaLink />}
+                                    isRound
+                                    size="lg"
+                                    m={2}
+                                    onClick={copyInviteLink}
+                                />
+                            </Tooltip>
+                        </Box>
+                    </Box>
+                </Box>
             </Stack>
         </>
     );
