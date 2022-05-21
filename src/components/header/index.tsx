71e586ab4c3d9c682e6f015a52b200a462dcc7a1
bhavan <bhavanl@techprovint.com>
2022-05-21 18:22:34 +0530

    responsive header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,23 +1,26 @@
+import { HamburgerIcon } from '@chakra-ui/icons';
 import {
     Avatar,
     Button,
     Flex,
     Heading,
     HStack,
+    IconButton,
     Menu,
     MenuButton,
     MenuItem,
     MenuList,
     Portal,
     useColorModeValue,
+    useMediaQuery,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { AiFillProfile, AiOutlineLogin } from 'react-icons/ai';
 import { HeaderProps, IconPropsExtended } from '../../interfaces';
 import { routes } from '../../routes';
-import { ColorModeSwitcher } from '../color-mode-switcher';
-import { AiFillProfile, AiOutlineLogin } from 'react-icons/ai';
-import styles from './header.module.css';
 import { colors } from '../../theme/colors';
+import { ColorModeSwitcher } from '../color-mode-switcher';
+import styles from './header.module.css';
 
 export const Logo = (props: IconPropsExtended) => {
     return (
@@ -32,6 +35,8 @@ export const Logo = (props: IconPropsExtended) => {
 };
 
 export const Header = (props: HeaderProps) => {
+    const [isPhoneScreen] = useMediaQuery('(max-width: 580px)');
+
     return (
         <header
             style={{
@@ -45,38 +50,60 @@ export const Header = (props: HeaderProps) => {
                 <Flex justifyContent="space-between" alignItems="center">
                     <nav>
                         <HStack className={styles.navStack} spacing={12} gap={5}>
-                            <Flex justifyContent="center" alignItems="center">
-                                <NextLink href="/" passHref>
-                                    <Button variant="ghost">
-                                        <Logo
-                                            style={{ margin: '0 10px' }}
-                                            size={50}
-                                            h="1.5rem"
-                                            pointerEvents="none"
-                                            mr={8}
-                                        />
-                                        <Heading size="lg">Sprint Planner</Heading>
-                                    </Button>
-                                </NextLink>
-                            </Flex>
-                            {routes.map(route => (
-                                <NextLink key={route.key} href={route.path}>
-                                    <Button
-                                        rightIcon={route.icon}
-                                        color={useColorModeValue(
-                                            colors.nav_button.light,
-                                            colors.nav_button.dark
-                                        )}
+                            {isPhoneScreen || (
+                                <Flex justifyContent="center" alignItems="center">
+                                    <NextLink href="/" passHref>
+                                        <Button variant="ghost">
+                                            <Logo
+                                                style={{ margin: '0 10px' }}
+                                                size={50}
+                                                h="1.5rem"
+                                                pointerEvents="none"
+                                                mr={8}
+                                            />
+                                            <Heading size="lg">Sprint Planner</Heading>
+                                        </Button>
+                                    </NextLink>
+                                </Flex>
+                            )}
+                            {isPhoneScreen ? (
+                                <Menu>
+                                    <MenuButton
+                                        as={IconButton}
+                                        aria-label="Options"
+                                        icon={<HamburgerIcon />}
                                         variant="outline"
-                                        width={'10em'}
-                                        sx={{
-                                            margin: '0 1em !important',
-                                        }}
-                                    >
-                                        {route.name}
-                                    </Button>
-                                </NextLink>
-                            ))}
+                                    />
+                                    <MenuList>
+                                        {routes.map(route => (
+                                            <NextLink key={route.key} href={route.path}>
+                                                <MenuItem icon={route.icon}>
+                                                    {route.name}
+                                                </MenuItem>
+                                            </NextLink>
+                                        ))}
+                                    </MenuList>
+                                </Menu>
+                            ) : (
+                                routes.map(route => (
+                                    <NextLink key={route.key} href={route.path}>
+                                        <Button
+                                            rightIcon={route.icon}
+                                            color={useColorModeValue(
+                                                colors.nav_button.light,
+                                                colors.nav_button.dark
+                                            )}
+                                            variant="outline"
+                                            width={'10em'}
+                                            sx={{
+                                                margin: '0 1em !important',
+                                            }}
+                                        >
+                                            {route.name}
+                                        </Button>
+                                    </NextLink>
+                                ))
+                            )}
                         </HStack>
                     </nav>
                 </Flex>
